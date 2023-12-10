9c74cceb25d0191039405a58358b7eee3e73b303
Jay Keer <85948257+Yolo-cell-hash@users.noreply.github.com>
2023-12-10 19:12:06 +0530

    Https integration

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const rosnodejs = require('rosnodejs');
-const http = require('http');
+const https = require('https');  // Switch to the 'https' module for secure requests
 
 // Initialize ROS node
 rosnodejs.initNode('/ros_listener_node')
@@ -10,15 +10,15 @@ rosnodejs.initNode('/ros_listener_node')
       sendDataToBackend(data);
     });
 
-    // Function to send data to the Node.js backend
+    // Function to send data to the HTTPS backend
     function sendDataToBackend(data) {
       // Create a JSON payload with the data
       const payload = JSON.stringify({ data: data });
 
-      // Configure the HTTP request options
+      // Configure the HTTPS request options
       const options = {
-        hostname: 'localhost',
-        port: 3000,
+        hostname: 'ill-pear-scorpion-vest.cyclic.app',
+        port: 443,  // Default port for HTTPS
         path: '/',
         method: 'POST',
         headers: {
@@ -27,8 +27,8 @@ rosnodejs.initNode('/ros_listener_node')
         },
       };
 
-      // Send the data to the backend server
-      const req = http.request(options, (res) => {
+      // Send the data to the HTTPS backend server
+      const req = https.request(options, (res) => {
         console.log(`Backend server responded with status code: ${res.statusCode}`);
       });
 
